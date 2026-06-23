5676e4ecc059502beaaf1fd3cd6e687fa1df0806
oriontasia <oriontasia@gmail.com>
2026-06-23 13:29:11 -0400

    Update crawler.rb

--- crawler.rb
+++ crawler.rb
@@ -4,7 +4,7 @@ system("rsync -avcizxL rsync.ietf.org::bibxml-ids ./bibxml-ids")
 
 require "relaton_ietf"
 
-FileUtils.rm Dir.glob("index*")
+FileUtils.rm Dir.glob("index 0abf9f338b99")
 FileUtils.rm_rf "data"
 
 RelatonIetf::DataFetcher.fetch "ietf-internet-drafts"
